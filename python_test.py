4916e6397499ee576de93b8bd2472551d8abcfb1
Perrshan <xaveperry@gmail.com>
2024-01-13 21:00:03 -0700

    first commit

---
+++ python_test.py
@@ -0,0 +1,20 @@
+def xor(p, q):
+    if p == True and q == True:
+        return False
+    elif p == True or q == True:
+        return True
+    else:
+        return False
+
+def truth_table_2_vars(function):
+    print(f"{function.__name__}")
+    print(f"{'p':<8}{'q':<8}{'ANS':<8}")
+    print("----------------------------------------")
+    rows = [(p,q) for p in (True, False) 
+                  for q in (True, False)]    
+    for p, q in rows:
+        ans = function(p,q)
+        print(f"{p!s:<8}{q!s:<8}{ans!s:<8}")
+    print()
+
+truth_table_2_vars(xor)
